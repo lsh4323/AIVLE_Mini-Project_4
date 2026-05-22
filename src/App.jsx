8f6eb8ce862e16911e04f00ff3facd7ec5445d7a
2themool <nh01240@naver.com>
2026-05-22 14:18:27 +0900

    도서 추가 return까지 한 듯??

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,28 @@
 import { useState, useEffect } from 'react';
-import BookForm from './components/BookForm'; // 수정1. 불러옴
+import BookForm from './components/BookForm'; 
 
 function App() {
   const [books, setBooks] = useState([]);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
 
+  // json-server 데이터 가져오기
   useEffect(() => {
     async function loadBooks() {
       try {
         const res = await fetch('http://localhost:3000/books');
+        if (!res.ok) throw new Error('서버 응답 오류');
         const data = await res.json();
         setBooks(data);
       } catch (err) {
         console.error(err);
-        setError('게시글을 불러오지 못했어요');
+        setError('데이터를 불러오지 못했습니다.');
       }
       setLoading(false);
     }
     loadBooks();
   }, []);
 
-  // 수정2.책 추가 함수
   const handleAddBook = async (newBook) => {
     try {
       const res = await fetch('http://localhost:3000/books', {
@@ -43,24 +44,32 @@ function App() {
     }
   };
   
-  // 책 삭제 함수
-  const handleDeleteBook = async (id) => {
-    
-  };
+  const handleDeleteBook = async (id) => { };
+  const handleUpdateBook = async (id, updatedBook) => { };
   
-  // 책 수정 함수
-  const handleUpdateBook = async (id, updatedBook) => {
+  if (loading) return <p>로딩 중...</p>;
+  if (error) return <p>에러: {error}</p>;
 
-  };
-  
-  if (loading) return <><p>불러오는 중...</p></>;
-  
-  if (error) return <><p>에러: {error}</p></>;
-
+  // return문 추가
   return (
-    <>
-      <h1>hellohihi</h1>
-    </>
+    <div>
+      <h1>도서관리시스템</h1>
+      
+      {/* 등록 폼 컴포넌트 */}
+      <BookForm onAddBook={handleAddBook} /> 
+
+      {/* 등록 테스트 확인용 목록 */}
+      <div>
+        <h3>도서 목록 ({books.length}권)</h3>
+        {books.map(b => (
+          <div key={b.id} style={{ border: '1px solid #000', margin: '5px', padding: '5px' }}>
+            <h4>{b.title}</h4>
+            <p>저자: {b.author}</p>
+            <p>내용: {b.content}</p>
+          </div>
+        ))}
+      </div>
+    </div>
   );
 }
 
